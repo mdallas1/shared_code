8c0c3c79de3359307f7463b8d2c47fb05b595e02
mdallas1 <mdallas256@gmail.com>
2025-02-09 13:43:50 -0600

    added secant method and fixed point example.

--- ex2_1_compare.m
+++ ex2_1_compare.m
@@ -1,13 +1,19 @@
+close all;
+M = 6000; v = 1000; n = 5;
+f = @(r) M - v * (1+r)./r .* ( (1+r).^n - 1);
+	
 [k_b,err_b] = ex2_1; 
 [k_n,err_n] = ex2_1_newt;
+[~,k_s,err_s] = secant(0.3,-0.3,f,50,1e-12);
 
 figure(1)
-semilogy(k_b,err_b,'b','LineWidth',1.5);
 hold on
-semilogy(k_n,err_n,'r','LineWidth',1.5);
-xlabel('Iteration','FontSize',13);
-legend('Bisection','Newton','FontSize',13);
+semilogy(k_b,err_b,'b','LineWidth',1.5);
+semilogy(k_n(1:end-1),err_n(2:end),'r','LineWidth',1.5);
+semilogy(k_s,err_s,'k','LineWidth',1.5);
+xlabel('Iteration','FontSize',20);
+legend('Bisection','Newton','Secant','FontSize',20);
 yticks([1e-16 1e-14 1e-12 1e-10 1e-8 1e-6 1e-4 1e-2 1]);
-set(gca,'FontSize',14)
+set(gca,'FontSize',20)
 figure(1)
 
---
+++ secant.m
@@ -0,0 +1,59 @@
+function [alpha,varargout] = secant(x0,x1,f,maxiters,tol,varargin)
+	%SECANT(x0,x1,f,maxiters,tol)
+	% Attempts to find a root of the function f given initial 
+	% iterates x0 and x1. Terminates after iterates exceed 
+	% maxiters or if the difference between consecutive iterates 
+	% is less than tol. 
+	% To see convergence history displayed in command window like 
+	% in the previous examples, type 'report' after tol when you 
+	% call SECANT, e.g., >>> secant(1,2,f,50,1e-8,'report') ;  
+	if !is_function_handle(f)
+		error('function argument must be a function handle.');
+	end
+
+	iters = 0; err = 1; tol = tol; 
+	maxiters = maxiters; err_arr = [];
+	x0 = x0; x1 = x1; 
+
+	if nargin > 5
+		fprintf("------------------------------\n")
+		fprintf("x_k \t \t |x_k-x_{k-1}|\n")
+		fprintf("------------------------------\n")
+	end
+		
+
+	while iters < maxiters && err > tol 
+		fx0 = f(x0); fx1 = f(x1); % function evals can be expensive. Just do them once. 
+		w = -(x1-x0)/(fx1-fx0) * fx1;
+	
+		x0 = x1; % need to update x0 and x1
+		x1 = x1 + w;
+
+		err = abs(w);
+		err_arr = [err_arr err];
+
+		if nargin > 5
+			fprintf("%.10f \t %0.3e \n",x1,err);
+		end
+			
+		iters += 1; 
+	end
+
+	if nargin > 5 && err < tol 
+		fprintf("==========================\n")
+		fprintf("Converged to %0.10f in %g iterations.\n",x1,iters);
+	elseif nargin > 5 
+		fprintf("==========================\n")
+		fprintf("Did not converge in %g iterations.\n",maxiters);
+	end
+	
+	
+	alpha = x1;		
+	varargout{1} = [1:iters];
+	varargout{2} = err_arr;
+	
+	 
+	
+	
+	
+	
