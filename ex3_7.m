6e2edca4b973bdd1b21e04dc480245c9e3e83622
mdallas1 <mdallas256@gmail.com>
2025-02-17 16:07:09 -0600

    fixed bugs in ex3_7

--- ex3_7.m
+++ ex3_7.m
@@ -9,11 +9,11 @@ function ex3_7()
 	close all;
 
 	f = @(x) x.*(x-2*pi).*exp(-x);
-	a = 0; b = 2*pi; n = 10; h = (b-a)/(n+1);
+	a = 0; b = 2*pi; n = 9; h = (b-a)/(n+1);
 	nodes = h *[0:n]; y = f(nodes);	
 
-	C = fft(y)/(n+1);
-	c = fftshift(fft(y))/(n+1);
+	C = fft(y)/(n+1); 
+	c = fftshift(C); % fftshift puts orders the coefficients like (c_{-(M+1)},...,c_{-1},c_0,...,c_M). The coefficient c_{M+1} is ommitted.  
 	
 	figure(1) 
 	hold on; grid on;
@@ -23,38 +23,19 @@ function ex3_7()
 	%plot(tt,interpft(y,1000),'linewidth',2.0); %interpft automatically plots interpolant
 	plot(t,trig_basis(t,c,n),'linewidth',2.0)
 
-
-
 	function out = trig_basis(x,c,n)
 		if rem(n,2) > 0 
 			mu = 1;	
-			cc = [c c(1)];
 		else 
 			mu = 0;
-			cc = c;
 		end
 		M = (n-mu)/2;
-	
 		% exponential form 	
 		e_sum = 0; mm = [-(M+mu):M];
+		c(1) = 0.5*c(1); % evidently we need to half c_{M+1} 
 		for k = 1:length(c)
 			e_sum += c(k)*exp(1i*mm(k)*x);
 		end
 		out = e_sum + mu*c(1)*exp(1i*(M+1)*x);
-
-		% sine and cosine form	
-		%A = flip(cc) + cc; B = i*(flip(cc)-cc);
-		%A = A(1:M+mu+1);
-		%B = B(1:M+mu+1);
-		%trig_sum = A(end)/2 ;
-		%for k = 1:M+mu
-		%	trig_sum += A(k)*cos((M-k+mu+1)*x)+B(k)*sin((M-k+mu+1)*x);
-		%end
-		%out = trig_sum ;
-			
-
-		
-
-
 
 		 
